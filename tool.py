e62ece4fe037cc0c57b7691b059f760d1aa243ed
shelei <shelei0601@qq.com>
2020-06-26 21:36:03 +0800

    ssh更新

--- tool.py
+++ tool.py
@@ -4,9 +4,12 @@ import sys
 import getopt
 import subprocess
 
+ssh_server_host = "123.57.229.246"
+
 def help():
     print("tool.py --cmd=runas [<param1>...] 管理员权限启动程序")
     print("tool.py --cmd=crash <pid> 主动崩溃某进程")
+    print("tool.py --cmd=sss 阿里云服务器IP")
 
 def main(argv):
     try:
@@ -19,12 +22,14 @@ def main(argv):
     if len(opts) == 0 and len(param) == 0:
         help()
         return
-        
+
     cmd = opts[0][1]
     if cmd == "runas":
         subprocess.call([cmd+"32.exe", ] + param)
     elif cmd == "crash":
         subprocess.call(["runas32.exe", "crash32.exe "] + param)
+    elif cmd == "sss":
+        print(ssh_server_host)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
